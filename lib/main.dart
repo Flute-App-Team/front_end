2f6f61b34d74a90887b1f8320aeb59856f380ec8
ilhamwp0701 <47767694+ilhamwp0701@users.noreply.github.com>
2022-04-20 10:41:07 +0700

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final url = "https://jsonplaceholder.typicode.com/posts";
+
+  var _postsJson = [];
+
+  void fetchPosts() async {
+    try {
+      final response = await get(Uri.parse(url));
+      final jsonData = jsonDecode(response.body) as List;
+
+      setState(() {
+        _postsJson = jsonData;
+      });
+    } catch (e) {}
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flute App'),
+        ),
+        body: ListView.builder(
+            itemCount: _postsJson.length,
+            itemBuilder: (context, i) {
+              final post = _postsJson[i];
+              return Text(
+                  "Title : ${post["title"]}\n Body : ${post["body"]}\n\n ");
+            }),
+      ),
+    );
+  }
+}
+
+// Learning
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Flute App'),
+//         ),
+//         body: Text('Body'),
+//         bottomNavigationBar: BottomNavigationBar(
+//           items: [
+//             BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+//             BottomNavigationBarItem(
+//                 label: 'Setting', icon: Icon(Icons.settings))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
